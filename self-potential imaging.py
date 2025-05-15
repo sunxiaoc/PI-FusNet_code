027bb28739cc9fc2b789c44a9ddf9f59b357197c
sunxiaoc <39732461+sunxiaoc@users.noreply.github.com>
2025-05-15 12:37:35 +0800

    Update self-potential imaging.py

--- self-potential imaging.py
+++ self-potential imaging.py
@@ -1,34 +1,29 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Sun Dec  3 18:03:49 2023
 
-@author: 噜啦啦
-"""
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import Polygon
 
-# 电荷概率成像反演程序
-# 由电势数据（10个电极点）计算电场数据（9个点），只能算x方向的E
+# Charge probability imaging inversion program
 def Cal_E(num,V):
     # V = pd.read_excel('D:/博士期间/联合反演/data01.xlsx',header=None)
-    # 假设中点的E可以这样计算
+    # Assuming that the midpoint E can be calculated as follows
     # V = np.array(V)
     Ex = []
     for i in range(num-1):
         Ex.append((V[0][i]-V[0][i+1])/0.3)
     return Ex
 
-# 线电荷模型的扫描函数
+# Scanning function of linear charge model
 def SDS(dx,zq):
     Sx = dx / (dx**2 + zq**2)**1.5
     return Sx
 
-# 概率成像函数
+# Probability imaging function
 def COP(xq,zq,Ex,num):
-    x = np.arange(1.65,3.4,0.3)  #比X少一个，X的中点坐标
-    # 对离散数据积分,即：trapz(值y,坐标x)
+    x = np.arange(1.65,3.4,0.3) 
+    # Integrating discrete data
     f1 = []
     f2 = []
     for i in range(num-1):
@@ -36,7 +31,7 @@ def COP(xq,zq,Ex,num):
         f2.append(Ex[i]**2)
     inte_1 = np.trapz(f1,x)
     inte_2 = np.trapz(f2,x)
-    # 计算概率
+    # Calculate probability
     C = 2*2**0.5 / (np.pi * inte_2)**0.5
     eta = C * zq**1.5 * inte_1
     return eta
@@ -44,7 +39,6 @@ def COP(xq,zq,Ex,num):
 # In[1]
 def ShowProbability_contourf(x, z, value):
     
-    # 注意：需要设置一下可绘制区域的位置和大小
     # levels = np.arange(-1,1,0.05)
     # levels = [-0.9, -0.8, -0.7, -0.6, -0.5, -0.4, -0.3, -0.2, -0.1,
     #           0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
@@ -62,9 +56,8 @@ def ShowProbability_contourf(x, z, value):
 # In[2]
 def main(V,X,Z,num):
     
-    # 电极中点位置处的Ex
+    # Ex at the midpoint of the electrode
     Ex = Cal_E(num,V)
-    # 计算结果
     r = []
     result = []
     for height in range(Z.shape[0]):
@@ -82,14 +75,13 @@ def main(V,X,Z,num):
 
 # In[3]
 # =============================================================================
-# 主程序运行
+# Main program running
 # =============================================================================
 if __name__ == '__main__':
     import os
     from scipy import interpolate
     path = 'E:/物理信息网络/数据集/现场数据/SP/'
     path2 = 'E:/物理信息网络/数据集/现场数据/SP_inv/'
-    # path3 = 'E:/物理信息网络/数据集/现场数据/SP_img/'
     pathdir = os.listdir(path)
     num = 7
     
